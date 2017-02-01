c3b736d6d4a3b44f98a74dfa4bd1b5f955715500
Ahmet Kursad DURAN <kduran@users.noreply.github.com>
2017-02-01 11:47:22 -0600

    Add files via upload

---
+++ FloatValue_DuranAhmet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void main() {
+	float point;
+	cin >> point;
+	cout << "Please enter a floating-point value : " << point << endl;
+	int ui;
+	ui = (int)point;
+	cout << "Whole number part is " << ui << endl;
+	system("pause");
+}
